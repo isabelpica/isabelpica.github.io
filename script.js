45765dbc46b378a3b765e11ade97f562005d7b62
isabelpica <60153223+isabelpica@users.noreply.github.com>
2020-04-28 13:09:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' Graphic Designer',
+        ' Creator',
+        ' Painter',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
